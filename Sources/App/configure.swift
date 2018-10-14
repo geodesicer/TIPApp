ac44c442f0292d2ce4173aed48b2a48320b3fe00
Ulf Olsson <ulf@distinct.se>
2018-10-14 23:28:23 +0200

    add siblings 2

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,26 +20,31 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Configure a SQLite database
     //let sqlite = try SQLiteDatabase(storage: .file(path: "db.sqlite"))
 
-    // 1
     var databases = DatabasesConfig()
-    // 2
-    let hostname = Environment.get("DATABASE_HOSTNAME")
-        ?? "localhost"
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD")
-        ?? "password"
-    // 3
+
+    let databaseName: String
+    let databasePort: Int
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432
+    }
+
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+
     let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
+        port: databasePort,
         username: username,
         database: databaseName,
         password: password)
-    // 4
+
     let database = PostgreSQLDatabase(config: databaseConfig)
-    // 5
     databases.add(database: database, as: .psql)
-    // 6
     services.register(databases)
     
     /// Configure migrations
